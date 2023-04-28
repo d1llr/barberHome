52069e10f6f716dd22596303ab5a2d9f5f6e2a35
d1llr <madramov.02@gmail.com>
2023-04-28 21:14:55 +0300

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import Layout from '@/components/layout/layoutWrapper/Layout'
-import { store } from '@/redux/store'
+import { store, wrapper } from '@/redux/store'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
   </Provider>
 }
+
+export default wrapper.withRedux(App)
+
--- src/redux/api/barberApi.ts
+++ src/redux/api/barberApi.ts
@@ -4,12 +4,20 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
 // Define a service using a base URL and expected endpoints
+const token = 'ctxn2pt2jwz2ushknuds'
 export const barberApi = createApi({
   reducerPath: 'barberApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.yclients.com/api/v1' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.yclients.com/api/v1', 
+  prepareHeaders: (headers) => {
+    // headers.set("Content-Type", "text/plain; charset=UTF-8");
+    headers.set('Accept', 'application/vnd.yclients.v2+json'),
+    headers.set('Authorization', `Bearer ${token}`),
+    console.log(headers);
+    return headers;
+  }, }),
   endpoints: (builder) => ({
     getStaff: builder.query<any, string>({
-      query: () => `company/${process.env.COMPANY_ID}/services/{service_id}`,
+      query: () => `book_services/256926`
     }),
   }),
 })
